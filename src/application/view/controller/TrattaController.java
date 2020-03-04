abb77a4bfb730bc59077d77a2fa7bc1604083a3f
ValeeeR <37934589+ValeeeR@users.noreply.github.com>
2020-03-04 12:08:10 +0100

    corretto

--- src/application/view/controller/TrattaController.java
+++ src/application/view/controller/TrattaController.java
@@ -1,13 +1,10 @@
 package application.view.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class TrattaController {
-
-	import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-	import javafx.scene.control.Button;
-	import javafx.scene.control.TextField;
-
-	public class PleaseProvideControllerClassName {
 
 	    @FXML
 	    private Button bottone_indietro;
