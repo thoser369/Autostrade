4f16451bfb494c989eded0274f55eb8927a8a2e7
ValeeeR <37934589+ValeeeR@users.noreply.github.com>
2020-03-04 12:07:12 +0100

    view Tratta

---
+++ src/application/view/controller/CaselloController.java
@@ -0,0 +1,83 @@
+package application.view.controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CaselloController {
+
+	    @FXML
+	    private Button bottone_indietro;
+
+	    @FXML
+	    private TextField txt21;
+
+	    @FXML
+	    private TextField txt210;
+
+	    @FXML
+	    private TextField txt29;
+
+	    @FXML
+	    private TextField txt28;
+
+	    @FXML
+	    private TextField txt27;
+
+	    @FXML
+	    private TextField txt26;
+
+	    @FXML
+	    private TextField txt25;
+
+	    @FXML
+	    private TextField txt24;
+
+	    @FXML
+	    private TextField txt22;
+
+	    @FXML
+	    private TextField txt23;
+
+	    @FXML
+	    private TextField txt31;
+
+	    @FXML
+	    private TextField txt310;
+
+	    @FXML
+	    private TextField txt39;
+
+	    @FXML
+	    private TextField txt38;
+
+	    @FXML
+	    private TextField txt37;
+
+	    @FXML
+	    private TextField txt36;
+
+	    @FXML
+	    private TextField txt35;
+
+	    @FXML
+	    private TextField txt34;
+
+	    @FXML
+	    private TextField txt33;
+
+	    @FXML
+	    private TextField txt32;
+
+	    @FXML
+	    private Button bottone_tratte;
+
+	    @FXML
+	    private Button bottone_aggiungiCasello;
+
+	 	@FXML
+	    void indietro(ActionEvent event) {
+
+	    }
+
+}
