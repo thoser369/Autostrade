6564fa57c1f41fafe5de0cba8ebfa91ae5683931
ValeeeR <37934589+ValeeeR@users.noreply.github.com>
2020-03-04 12:52:25 +0100

    ImpostazioniController, path nelle view per il collegamento al Controller

---
+++ src/application/view/controller/ImpostazioniController.java
@@ -0,0 +1,20 @@
+package application.view.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+
+public class ImpostazioniController {
+
+	 @FXML
+	    private Button bottone_autostrade;
+
+	    @FXML
+	    private Button bottone_profilo;
+
+	    @FXML
+	    private Button bottone_tariffe;
+
+	    @FXML
+	    private Button bottone_aggiungi;
+
+}
