ed0c0b56248ff260c1f086f6c550e6d165949d2f
ValeeeR <37934589+ValeeeR@users.noreply.github.com>
2020-03-04 12:26:24 +0100

    Update TariffeController.java

--- src/application/view/controller/TariffeController.java
+++ src/application/view/controller/TariffeController.java
@@ -1,5 +1,35 @@
 package application.view.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class TariffeController {
 
-}
+	 @FXML
+	    private TextField txtnome;
+
+	    @FXML
+	    private TextField txtnome1;
+
+	    @FXML
+	    private TextField txtnome2;
+
+	    @FXML
+	    private Button bottone_indietro;
+
+	    @FXML
+	    private Button bottone_conferma;
+
+	    @FXML
+	    void conferma_tariffa(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void indietro(ActionEvent event) {
+
+	    }
+
+	}
