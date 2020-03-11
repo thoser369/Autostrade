2b6c545df03a50fa9163954f076d62d5585cacf9
thoser369 <38078706+thoser369@users.noreply.github.com>
2020-03-11 18:58:36 +0100

    Controller calcolo pedaggio

---
+++ src/application/view/controller/CalcoloDelPedaggioController.java
@@ -0,0 +1,33 @@
+package application.view.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalcoloDelPedaggioController {
+
+    @FXML
+    private Label label_classe;
+
+    @FXML
+    private Button bottone_pedaggio;
+
+    @FXML
+    private TextField txtpedaggio;
+
+    @FXML
+    private Button bottone_indietro;
+
+    @FXML
+    void calcolo_pedaggio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void indietro(ActionEvent event) {
+
+    }
+
+}
