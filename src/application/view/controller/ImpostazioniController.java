2da22f313d7e0b5dd20b0d04edd611855244ac1e
thoser369 <38078706+thoser369@users.noreply.github.com>
2020-03-11 19:11:51 +0100

    Controller Impostazioni

--- src/application/view/controller/ImpostazioniController.java
+++ src/application/view/controller/ImpostazioniController.java
@@ -1,7 +1,15 @@
 package application.view.controller;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ImpostazioniController {
 
@@ -9,9 +17,48 @@ public class ImpostazioniController {
     private Button bottone_AutostradeECaselli;
 
     @FXML
+    private Button bottone_tariffe;
+
+    @FXML
     private Button bottone_pedaggio;
 
     @FXML
-    private Button bottone_tariffe;
+    void scelta_AutostradeECaselli(ActionEvent event) throws IOException {
+    	((Node)event.getSource()).getScene().getWindow().hide(); 
+		Stage primaryStage = new Stage();
+		FXMLLoader loader = new FXMLLoader();
+		AnchorPane root=loader.load(getClass().getResource("/application/view/fxml/AutostradeECaselli.fxml").openStream());
+		Scene scene = new Scene(root);							
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Gestione Autostrade e Caselli");
+		primaryStage.show();
+
+    }
+
+    @FXML
+    void scelta_GestionePedaggio(ActionEvent event) throws IOException {
+    	((Node)event.getSource()).getScene().getWindow().hide(); 
+		Stage primaryStage = new Stage();
+		FXMLLoader loader = new FXMLLoader();
+		AnchorPane root=loader.load(getClass().getResource("/application/view/fxml/GestioneClassePedaggio.fxml").openStream());
+		Scene scene = new Scene(root);							
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Gestione Calcolo Pedaggio");
+		primaryStage.show();
+
+    }
+
+    @FXML
+    void scelta_GestioneTariffe(ActionEvent event) throws IOException {
+    	((Node)event.getSource()).getScene().getWindow().hide(); 
+		Stage primaryStage = new Stage();
+		FXMLLoader loader = new FXMLLoader();
+		AnchorPane root=loader.load(getClass().getResource("/application/view/fxml/Tariffe.fxml").openStream());
+		Scene scene = new Scene(root);							
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Gestione Tariffe");
+		primaryStage.show();
+
+    }
 
 }
